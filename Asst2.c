ef714bf923d9f26ef71684b1ee4bbe607c5a1080
mvaysiberg <mvays11@gmail.com>
2020-11-25 22:45:43 -0500

    main file

---
+++ Asst2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <cmath.h>
+
